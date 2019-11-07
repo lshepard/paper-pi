2b8ca97384c3279a47c9a84213e70ac807d70bf8
Luke Shepard <luke@esparklearning.com>
2019-11-07 14:58:17 -0600

    adding the sample code

---
+++ src/google-sample.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import datetime
+
+# If modifying these scopes, delete the file token.pickle.
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+
+if __name__ == '__main__':
+    main()
+    
